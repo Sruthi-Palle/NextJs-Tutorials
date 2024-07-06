2199c1939038901a4ada5659c96110e765078e39
Sruthi-Palle <pallesruthi12@gmail.com>
2024-07-06 14:47:26 +0530

    Restructured folders

---
+++ Nextjs-Concepts/nested-routes/src/app/dashboard/settings/page.tsx
@@ -0,0 +1,11 @@
+// /app/dashboard/settings/page.tsx
+const SettingsPage = () => {
+  return (
+    <div className="m-4">
+      <h1>Settings</h1>
+      <p>Settings content goes here...</p>
+    </div>
+  );
+};
+
+export default SettingsPage;
---
+++ Nextjs-Concepts/nested-routes/src/app/loading.tsx
@@ -0,0 +1,4 @@
+// app/loading.js
+export default function Loading() {
+  return <div>Loading...</div>;
+}
